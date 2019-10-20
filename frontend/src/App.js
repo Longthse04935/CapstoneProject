34c06fbfb770637067db67c64e9443df98ddb41c
Long <longthse04935@gmail.com>
2019-10-20 15:53:33 +0700

    20/10

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,24 @@
-import React, { Component } from 'react';
-import Chatbox from './Chatbox';
+import React, { Component } from "react";
+import Chatbox from "./Chatbox";
+import EditPost from "./EditPost";
+import ProfileGuiders from "./ProfileGuiders";
+import Tour from "./Tour";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 class App extends Component {
-    
-    render() {
-        return (
-            <div>
-                <Chatbox/>
-            </div>
-        );
-    }
+  render() {
+    return (
+        <Router>
+        <div>
+          <Switch>
+              <Route exact path='/chatbox' component={Chatbox} />
+              <Route path='/editpost' component={EditPost} />
+              <Route path='/profileguiders' component={ProfileGuiders} />
+              <Route path='/tour' component={Tour} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
-

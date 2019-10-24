f43c008dcd4d9f96bdcd725299e35b7907876a77
duongpt4497 <duongptse05022@fpt.edu.vn>
2019-10-24 07:23:55 +0700

    login logout signup

--- src/App.js
+++ src/App.js
@@ -9,27 +9,52 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Navbar from './Navbar';
 import Footer from './Footer';
 import Logged from './Logged';
+import Cookies from 'js-cookie'
+
 class App extends Component {
-  render() {
-    return (
-        <Router>
-        <div>
-        <Logged />
-        {/* <Navbar /> */}
-          <Switch>
+	constructor(props) {
+		super(props);
+		this.state={logedIn: false};
+		
+	  }
+
+	reload = () => {
+		this.setState({logedIn: !this.state.logedIn});
+	}
+
+	sample = () => {
+		return 1+1;
+	}
+
+	render() {
+	
+		let present = this.state.logedIn ?  <Logged reload={this.reload}/>:<Navbar reload={this.reload}/>;
+		
+		
+		return (
+			<div>
+
+
+				{present}
+				<Router>
+
+
+
+					{/* <Switch>
               <Route exact path='/chatbox' component={Chatbox} />
               <Route path='/editpost' component={EditPost} />
-              <Route path='/profileguiders' component={ProfileGuiders} >
-              </Route>
+              <Route path='/profileguiders' component={ProfileGuiders} />
               <Route path='/tour' component={Tour} />
               <Route path='/profiletraveller' component={ProfileTravaller} />
-              <Route path={"/"}><Home guiderId={4} /></Route>
-          </Switch>
-          <Footer />
-        </div>
-      </Router>
-    );
-  }
+              <Route exact path={"/"}><Home guiderId={4} /></Route>
+          </Switch> */}
+
+
+				</Router>
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default App;
